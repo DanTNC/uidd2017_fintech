573c06384574b8090de66f93639c6b61a76ca7d9
DanTNC <fad11204@yahoo.com.tw>
2017-03-10 01:33:45 +0800

    2017/3/10

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,40 @@
 // Empty JS for your own code to be here
 
-$('document').ready(function(){
+$(document).ready(function(){
     $('.carousel.slide').carousel({
         interval: 5000,
         cycle: true
-    }); 
+    });
+
+    $(window).scroll(function(){
+        if($(window).scrollTop()!=0){
+            $('.navbar').css("padding-top","0pt");
+            $('.navbar').css("padding-bottom","10pt");
+            $('.navbar').css("background-color","#231f20");
+        }else{
+            $('.navbar').css("padding-top","50pt");
+            $('.navbar').css("padding-bottom","0pt");
+            $('.navbar').css("background-color","rgba(0, 0, 0, 0)");
+        }
+    });
+
+    $('#Google_Play').mouseover(function(){
+        $('#Google_Play img').attr("src","img/Google Play hover.png");
+    });
+    $('#Apple_Store').mouseover(function(){
+        $('#Apple_Store img').attr("src","img/Apple Store hover.png");
+    });
+
+    $('#Google_Play').mouseout(function(){
+        $('#Google_Play img').attr("src","img/Google Play.png");
+    });
+    $('#Apple_Store').mouseout(function(){
+        $('#Apple_Store img').attr("src","img/Apple Store.png");
+    });
 })
+
+/*
+    padding-top: 0pt;
+    padding-bottom: 10pt;
+    background-color: #231f20;
+*/
