758989d79da2bbed164829822e1a8eda54b5f414
DanTNC <fad11204@yahoo.com.tw>
2017-03-15 17:24:38 +0800

    git width

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ if ('webkitSpeechRecognition' in window) {
     recognition.lang="en-US";
     //recognition.lang="cmn-Hant-TW";
     recognition.onstart = () => {
-        $(".alert").html('<img src="https://media.giphy.com/media/xTk9ZGzPbqz5cP9Zg4/giphy.gif">');
+        $(".alert").html('<img src="https://media.giphy.com/media/xTk9ZGzPbqz5cP9Zg4/giphy.gif" style="width:100%;">');
         $("#myModal").modal();
         recognition_text = "";
         console.log('start recognition...');
